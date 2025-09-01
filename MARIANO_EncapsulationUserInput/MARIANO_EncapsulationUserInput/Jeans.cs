04c7373285c5418332706bce3dc3ab18ba1d3ffe
ACT-STUDENT <ACT-STUDENT@LAB3-PC24>
2025-09-01 10:30:37 +0800

    exer5 program

---
+++ MARIANO_EncapsulationUserInput/MARIANO_EncapsulationUserInput/Jeans.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_EncapsulationUserInput
+{
+    internal class Jeans
+    {
+        // Declaring the class fields
+        private string brand;
+        private string size;
+
+        // This is the setter for the brand
+        public string Brand
+        {
+            get { return brand; }
+            set { brand = value; }
+
+        }
+        // This is the setter for the size
+        public string Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+        // THis is the method of the Jeans fit
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Brand:           {Brand},    Size:     {Size}");
+
+        }
+
+    }
+}
---
+++ MARIANO_EncapsulationUserInput/MARIANO_EncapsulationUserInput/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_EncapsulationUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Name: Savana Mariano
+            // Section: IT301
+            // EXer Title: Basics on Encapsulation
+
+
+            // This is the instanx=ce of the Jeans class
+            Jeans jeans1 = new Jeans();
+
+            // This is the properties using setter method
+            Console.WriteLine("Input the Brand of the Jeans:       ");
+            jeans1.Brand = Console.ReadLine();
+
+            Console.WriteLine("Input the Size of the Jeans:        ");
+            jeans1.Size = Console.ReadLine();
+
+
+            // Display Jeans information using method
+            jeans1.DisplayInfo();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
