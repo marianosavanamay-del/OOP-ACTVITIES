bcba4b5aaf4f096261ed696242a4171ebd5fa85c
ACT-STUDENT <ACT-STUDENT@LAB3-PC24>
2025-09-01 10:23:28 +0800

    exer4 program

---
+++ MARIANO_PropertiesAndEncapsulation/MARIANO_PropertiesAndEncapsulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_PropertiesAndEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is the instance of Jeans class
+            Jeans jeans1 = new Jeans();
+
+            // This is the properties using the setter method\
+            jeans1.Brand = "Uniqlo";
+            jeans1.Size = "Small";
+
+
+            // Display Jeans information using method
+            jeans1.DisplayInfo();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
