8a72dbef60b7ab69a70a2755327a4ee9a9fb4eb5
ACT-STUDENT <ACT-STUDENT@LAB3-PC13>
2025-09-05 10:18:12 +0800

    program exer6

---
+++ MARIANO_SimpleInheritance/MARIANO_SimpleInheritance/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_SimpleInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // NAme: Savana Mariano
+            // Section: IT301
+            // Exer Tittle: Simple Inheritance with Properties
+
+            // Instance of Regullar and Irregular Student
+
+
+            RegularStudent regular = new RegularStudent
+            {
+                Name = "Savana Mariano",
+                Program = "BS in Information and Technology",
+                Section = "IT301",
+            };
+            //Method specific to Regular Student
+            Console.WriteLine("Regular Student Info");
+            regular.BasicInfo();
+            regular.SectionEnrolled();
+
+            Console.WriteLine();
+            IrregularStudent irregular = new IrregularStudent
+            {
+                Name = "Conrad Fisher",
+                Program = "BS in Information and Technology",
+                UnitsEnrolled = "20",
+            };
+            //Method Specific to Irregular student
+            Console.WriteLine("Irregular Student Info");
+            irregular.BasicInfo();
+            irregular.EnrolledSemUnits();
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ MARIANO_SimpleInheritance/MARIANO_SimpleInheritance/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_SimpleInheritance
+{
+    internal class Student
+    {
+        // This is the method of the base class
+        public string Name { get; set; }
+        public string Program { get; set; }
+
+        public void BasicInfo()
+        {
+            Console.WriteLine($"Name:     {Name}");
+            Console.WriteLine($"Program:  {Program}");
+
+        }
+    }
+    class RegularStudent : Student  // Derived Class
+    {
+        // Method specific to Student class
+        public string Section { get; set; }
+
+        public void SectionEnrolled()
+        {
+            Console.WriteLine($"Section:   {Section}");
+        }
+
+    }
+    class IrregularStudent : Student     // Derived Class
+    {
+        // Method specific to Irregular class
+        public string UnitsEnrolled { get; set; }
+
+        public void EnrolledSemUnits()
+        {
+            Console.WriteLine($"Units this Semester:   {UnitsEnrolled}");
+        }
+    }
+
+
+}
+
