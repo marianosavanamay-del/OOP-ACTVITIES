84133459f3662e476c640dc369d60d99e5bfb508
ACT-STUDENT <ACT-STUDENT@LAB1-PC33>
2025-09-16 09:23:38 +0800

    exer 8

---
+++ MARIANO__InheritanceUserInput/MARIANO__InheritanceUserInput/Jeans.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO__InheritanceUserInput
+{
+	internal class Jeans
+	{
+		public string Brand { get; set; }
+
+		// display the brand of the jeans
+		public void BrandInfo()
+		{
+			Console.Clear();
+			Console.Write("Brand:    ");
+			Console.WriteLine("Brand");
+		}
+	}
+	class BaggyJeans : Jeans  // First derived class
+	{
+		public string Name { get; set; }
+		public string Color { get; set; }
+
+		//Display the Baggy jeans details including together with brand, name, and color
+		public void BaggyJeansInfo()
+		{
+
+			Console.Clear();
+			Console.WriteLine("Baggy Jeans Details:    ");
+			Console.WriteLine();
+			Console.Write("Brand:  ");
+			Console.WriteLine(Brand);
+			Console.Write("Name:   ");
+			Console.WriteLine(Name);
+			Console.Write("Color:  ");
+			Console.WriteLine(Color);
+
+		}
+	}
+	class FlaredJeans : Jeans
+	{
+		public string Size { get; set; }
+		public string Price { get; set; }
+
+		//dzisplay Faled jeans Details together with brand, size and prize
+		public void FlaredJeansInfo()
+		{
+			Console.Clear();
+			Console.WriteLine("Flared Jeans Details:   ");
+			Console.WriteLine();
+			Console.Write("Brand:  ");
+			Console.WriteLine(Brand);
+			Console.Write("Size:  ");
+			Console.WriteLine(Size);
+			Console.Write("Price:  ");
+			Console.WriteLine(Price);
+		}
+
+
+	}
+
+}
+
+	
+
---
+++ MARIANO__InheritanceUserInput/MARIANO__InheritanceUserInput/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO__InheritanceUserInput
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Name: Savana Mariano
+			//Section: It301
+			// Exer Title: Inheritance with User Input
+
+			// this is where the user will choose which item he/she likes
+			Console.WriteLine("Please choose a Number:  ");
+			Console.WriteLine("1 - Baggy Jeans, 2 - Flared Jeans");
+			int choice = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter the Details of your chosen Jeans:   ");
+			Console.WriteLine("");
+
+			if (choice == 1)
+			{
+				//User Input for Baggy Jeans
+				BaggyJeans baggyJeans1 = new BaggyJeans();
+				Console.Write("Please Enter the Brand:  ");
+				string brand = Console.ReadLine();
+
+				Console.Write("Enter the Name:   ");
+				string name = Console.ReadLine();
+
+				Console.Write("Enter the Color:   ");
+				string color = Console.ReadLine();
+
+				// Set the properties for the Baggy Jeans Object
+				baggyJeans1.Brand = brand;
+				baggyJeans1.Name = name;
+				baggyJeans1.Color = color;
+
+				//display the BaggyJeans info
+				Console.WriteLine();
+				Console.Write("Here's the Details of your Order:   ");
+				Console.WriteLine();
+				baggyJeans1.BaggyJeansInfo();
+
+			}
+
+			// If the user chooses Flared Jeans, get its details from user input
+			else if (choice == 2)
+			{
+				//User input for Flared Jeans
+				FlaredJeans flaredJeans1 = new FlaredJeans();
+				Console.Write("Please Enter the Brand:  ");
+				string brand = Console.ReadLine();
+
+				Console.Write("Enter the Size:  ");
+				string size = Console.ReadLine();
+
+				Console.Write("Enter the Price:  ");
+				string price = Console.ReadLine();
+
+				// Set the properties for the Flared Jeans objects
+				flaredJeans1.Brand = brand;
+				flaredJeans1.Size = size;
+				flaredJeans1.Price = price;
+
+				//Display the flared jeans info
+				Console.WriteLine();
+				Console.WriteLine("Here's the Details of your Order:  ");
+				Console.WriteLine();
+				flaredJeans1.FlaredJeansInfo();
+			}
+			else
+			{
+				Console.WriteLine("Input Again");
+
+			}
+
+			Console.ReadKey();
+		}
+
+	}
+}
+
