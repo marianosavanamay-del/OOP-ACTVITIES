ee9308679f80bdbe4e574cc7fa830fefd2630fb8
ACT-STUDENT <ACT-STUDENT@LAB1-PC33>
2025-09-16 09:16:22 +0800

    exer 7

---
+++ MARIANO__InheritanceWithConstructor/MARIANO__InheritanceWithConstructor/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO__InheritanceWithConstructor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//NAme: Savana Mariano
+			//Section: IT301
+			// Exer Title: Simple Inheritance with Constructor
+
+			// object for the Regualr Student
+			RegularStudent regularStudent = new RegularStudent("Conrad Fisher", "Cauliflower", "Bachelor of Science in Medical Technology");
+
+			// object for the Irregular student
+			IrregularStudent irregularStudent = new IrregularStudent("Kaiuz Sevilleno", "Bachelor of Science in Industrial Engineering", 20);
+
+
+			Console.WriteLine("Regular Student");
+			regularStudent.StudentBasicInfo();
+			regularStudent.SectionEnrolled();
+
+
+
+			Console.WriteLine();
+			Console.WriteLine("Irregular Student");
+			irregularStudent.StudentBasicInfo();
+			irregularStudent.EnrolledSemUnits();
+
+
+			Console.ReadKey();
+
+		}
+	}
+}
---
+++ MARIANO__InheritanceWithConstructor/MARIANO__InheritanceWithConstructor/Student.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO__InheritanceWithConstructor
+{
+
+	class Student // This is the base class
+	{
+		public string Name { get; set; }
+
+		public string Program { get; set; }
+
+
+
+		// This is the constructor
+		public Student(string name, string program)
+		{
+			Name = name;
+			Program = program;
+
+		}
+
+		// This is the method to display Student Info
+		public void StudentBasicInfo()
+		{
+			Console.WriteLine($"Name:      {Name}");
+			Console.WriteLine($"Program:  {Program}");
+		}
+	}
+
+	class RegularStudent : Student  // This is the derived class
+	{
+
+		public string Section { get; set; }
+
+		// This is the constructor of the Derived class
+		public RegularStudent(string name, string program, string section) : base(name, program)
+		{
+
+			Section = section;
+		}
+		//Method to display Regular student info
+		public void SectionEnrolled()
+		{
+
+			Console.WriteLine($"Section:  {Section}");
+		}
+
+	}
+	class IrregularStudent : Student  // Also a derived class
+	{
+
+		public int UnitsEnrolled { get; set; }
+
+		//Constructor of the derived class
+		public IrregularStudent(string name, string program, int units) : base(name, program)
+		{
+
+			UnitsEnrolled = units;
+		}
+		// Method to display Irregular student info
+		public void EnrolledSemUnits()
+		{
+
+			Console.WriteLine($"UnitsEnrolled:  {UnitsEnrolled}");
+
+
+
+			Console.ReadLine();
+		}
+
+	}
+
+}
