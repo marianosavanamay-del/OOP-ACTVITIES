3fb29030dc57612362b88abae3183a6428d8fc7a
ACT-STUDENT <ACT-STUDENT@LAB3-PC03>
2025-09-24 08:41:39 +0800

    exer 2

---
+++ MARIANO_MyClassAndObjects/MARIANO_MyClassAndObjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_MyClassAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name: Savana Mariano
+            //Section: IT301
+            //Title: MyClassAndObjects
+
+            //This is the object of my classs
+            animal animal1 = new animal();
+            animal animal2 = new animal();
+
+            //This is the values for the fields
+            animal1.mammal = "Dog";
+            animal1.breed = "Golden Retriever";
+            animal1.age = 1;
+
+            animal2.mammal = "Cat";
+            animal2.breed = "Persian Cat";
+            animal2.age = 3;
+
+            //this is to call the method\
+            animal1.info();
+            animal2.info();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ MARIANO_MyClassAndObjects/MARIANO_MyClassAndObjects/animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_MyClassAndObjects
+{
+    internal class animal
+    {
+        //This is the fields
+        public string mammal;
+        public string breed;
+        public int age;
+
+        public void info()
+        {
+            //This is where the method to display the info
+            Console.WriteLine();
+            Console.WriteLine("mammal: " + mammal);
+            Console.WriteLine("breed: " + breed);
+            Console.WriteLine("age: " + age);
+        }
+    }
+}
