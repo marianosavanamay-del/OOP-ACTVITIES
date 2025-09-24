c756ae614dee7c489858f110a19acc3abaec1a56
ACT-STUDENT <ACT-STUDENT@LAB3-PC03>
2025-09-24 08:51:45 +0800

    exer 1

---
+++ MARIANO_CaO/MARIANO_CaO/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_CaO
+{
+    internal class Class1
+    {
+        //fields
+        public string name, gender;
+        public int age;
+
+        public void greet()
+        {
+            Console.WriteLine("Hello " + name + " Rise n Shine!");
+            Console.WriteLine("Your " + age + " yrs old " + "and " + gender);
+        }
+    }
+}
---
+++ MARIANO_CaO/MARIANO_CaO/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_CaO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Name: Savana Mariano
+            // Section: IT301
+            // Exercise title: Creating a Class
+
+            // Instantiate a person
+            Class1 person = new Class1();
+
+            // Initialized values for the fields
+            person.age = 19;
+            person.name = "Savana Mariano";
+            person.gender = "Female";
+
+            // method to display greetings
+            person.greet();
+            Console.ReadKey();
+        }
+    }
+}
