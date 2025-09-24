71fc466fd47bafe8a63da620a5194c1a4b354a62
ACT-STUDENT <ACT-STUDENT@LAB3-PC03>
2025-09-24 08:32:58 +0800

    exer 3

---
+++ MARIANO_COConstructorUserInput/MARIANO_COConstructorUserInput/Animal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_COConstructorUserInput
+{
+    internal class Animal
+    {
+        public string mammal;
+        public string breed;
+        public int age;
+
+        public Animal(string aMammal, string aBreed, int aAge) //Parameterized Constructor
+        {
+            mammal = aMammal;
+            breed = aBreed;
+            age = aAge;
+        }
+
+        public void DisplayAnimalInfo()
+        {
+            Console.Clear();
+            Console.WriteLine("These are the Animal details");
+            Console.WriteLine();
+            Console.WriteLine("Mammal:" + mammal);
+            Console.WriteLine("Breed:" + breed);
+            Console.WriteLine("Age:" + age);
+        }
+    }
+}
---
+++ MARIANO_COConstructorUserInput/MARIANO_COConstructorUserInput/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MARIANO_COConstructorUserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name: Savana Mariano
+            //Section: IT301
+            //Exercise Title: Create a Class
+
+            //This is the user input
+            Console.WriteLine("Hello! Are you fond of puppies and kittens?");
+            Console.WriteLine("These are our adorable pets!");
+            Console.WriteLine("They are so playful and friendly, I can suggest which pet are you choosing");
+            Console.WriteLine();
+
+            Console.Write("Enter mammal: ");
+            string aMammal = Console.ReadLine();
+
+            Console.Write("Enter breed: ");
+            string aBreed = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            int Age = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Feel free to chose what pet you adore");
+
+            //Initialize a method
+            Animal animal = new Animal(aMammal, aBreed, Age);
+
+            //Call the object to display the object details
+            animal.DisplayAnimalInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
